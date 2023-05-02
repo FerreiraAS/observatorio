357ac4f84e54e9c5f62645083937d65828228814
Arthur Ferreira <arthur_sf@icloud.com>
2023-05-02 08:57:11 -0300

    Commit.

--- Scripts/get-metrics-from-dois.R
+++ Scripts/get-metrics-from-dois.R
@@ -1,5 +1,5 @@
 # Verifica se há DOIs
-if (is_empty(dois)) {
+if (sjmisc::is_empty(dois)) {
   doi_unique <- as.data.frame(matrix(nrow = 0, ncol = 0))
   my_dois_works <- list()
 } else {
@@ -9,17 +9,28 @@ if (is_empty(dois)) {
   source("Scripts/altmetric-from-dois.R", local = knitr::knit_global())
   doi_unique <- doi_reshaped_data
   
+  # add citation counts
+  doi_unique$citations <- rep(0, dim(doi_unique)[1])
+  for (i in 1:dim(doi_unique)[1]) {
+    try({
+      citations <-
+        rcrossref::cr_citation_count(doi = as.character(doi_unique$doi[i]), key = "cienciasdareabilitacao@souunisuam.com.br")
+      doi_unique$citations[i] <- citations$count
+    },
+    silent = TRUE)
+  }
+  
   # get data of papers without Altmetric from CrossRef
-  if (is_empty(no_altmetric_dois_list)) {
+  if (sjmisc::is_empty(no_altmetric_dois_list)) {
     my_dois_works <- list()
   } else {
     my_dois_works <-
       rcrossref::cr_works(dois = unlist(no_altmetric_dois_list)) %>%
-      pluck("data")
+      purrr::pluck("data")
     if (length(my_dois_works) != 0) {
       # sort columns by title
       my_dois_works <-
-        my_dois_works[order(as.vector(my_dois_works$title)),]
+        my_dois_works[order(as.vector(my_dois_works$title)), ]
       # replace is_oa from Crossref
       for (i in 1:length(my_dois_works$doi)) {
         my_doi_oa <-
@@ -29,20 +40,31 @@ if (is_empty(dois)) {
                  as.character(my_doi_oa$is_oa),
                  "false")
       }
+      # add citation counts
+      my_dois_works$citations <- rep(0, dim(my_dois_works)[1])
+      for (i in 1:dim(my_dois_works)[1]) {
+        try ({
+          citations <-
+            rcrossref::cr_citation_count(doi = as.character(my_dois_works$doi[i]),
+                                         key = "cienciasdareabilitacao@souunisuam.com.br")
+          my_dois_works$citations[i] <- citations$count
+        },
+        silent = TRUE)
+      }
+      # add issued (created) year of publication
+      tryCatch(
+        expr = {
+          # get year from issued
+          my_dois_works$published_on <-
+            stringr::str_sub(as.character(my_dois_works$issued), 1, 4)
+          # get year from created if not issued
+          my_dois_works$published_on[is.na(my_dois_works$published_on)] <-
+            stringr::str_sub(as.character(my_dois_works$created[is.na(my_dois_works$published_on)]), 1, 4)
+        },
+        error = function(e) {
+          
+        }
+      )
     }
   }
-  # add issued (created) year of publication
-  tryCatch(
-    expr = {
-      # get year from issued
-      my_dois_works$published_on <-
-        str_sub(as.character(my_dois_works$issued), 1, 4)
-      # get year from created if not issued
-      my_dois_works$published_on[is.na(my_dois_works$published_on)] <-
-        str_sub(as.character(my_dois_works$created[is.na(my_dois_works$published_on)]), 1, 4)
-    },
-    error = function(e) {
-      
-    }
-  )
 }
