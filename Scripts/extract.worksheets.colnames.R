ca9a4d9a6a5bdea50552009bc1d4e5548f29a476
Arthur Ferreira <arthur_sf@icloud.com>
2023-05-11 18:27:52 -0300

    Commit.

--- Scripts/extract.worksheets.colnames.R
+++
@@ -1,9 +0,0 @@
-# READ XLSX FILES
-filename <- "conferencia_programa.xlsx"
-sheets <- readxl::excel_sheets(filename)
-
-for (i in 1:length(sheets)){
-	headers <- names(data.frame(read_excel("conferencia_programa.xlsx", sheet = sheets[i])))
-	cat(noquote(paste0("- ", sheets[i], "\n")))
-	cat(noquote(paste0("-- ", headers, "\n")))
-}
