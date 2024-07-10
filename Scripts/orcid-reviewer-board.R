346ec020b2fcac78d8f2ea1598ccc5ed4655ef54
Arthur Ferreira <arthur_sf@icloud.com>
2024-07-09 21:26:54 -0300

    Commit.

--- Scripts/orcid-reviewer-board.R
+++ Scripts/orcid-reviewer-board.R
@@ -15,7 +15,7 @@ if (is.null(res[[1]]$group$`external-ids.external-id`)) {
   # list journals
   journals <- rorcid::issn_title[issn]
   
-  # get SJR and CiteScore from database
+  # get SJR and CiteScore
   SJR <- c()
   CiteScore <- c()
   for (i in 1:length(issn)) {
